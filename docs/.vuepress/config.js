b8e994c0d096ace877747eb9fe5ed886cdb2e971
Bart Ledoux <ledouxb@me.com>
2018-10-20 09:57:11 +0200

    add vuepress to generate the documentation

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	title: 'Styleguidist Docs',
+	themeConfig: {
+		sidebar: [
+			['/', 'Introduction'],
+			'/API',
+			'/CLI',
+			['/Components', 'Components'],
+			'/Configuration',
+			'/Cookbook',
+			'/Development',
+			'/Documenting',
+			'/GettingStarted',
+			'/Webpack',
+		],
+	},
+};
