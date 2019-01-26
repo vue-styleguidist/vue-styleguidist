ed781ac89745015b85bf3f2d4b8c639e449361f3
Bart Ledoux <ledouxb@me.com>
2019-01-26 10:34:53 -0600

    fix: more DRY server for cypress

--- test/run.server.js
+++ test/run.server.js
@@ -4,40 +4,18 @@ const styleguidist = require('../scripts');
 
 /* eslint-disable no-console */
 
-const dir = path.resolve(__dirname, '../examples/basic/src');
+const dir = path.resolve(__dirname, '../examples/basic');
+const config = require(path.join(dir, 'styleguide.config'));
 
-styleguidist({
-	components: path.resolve(dir, 'components/**/[A-Z]*.vue'),
-	webpackConfig: {
-		module: {
-			rules: [
-				{
-					test: /\.vue$/,
-					loader: 'vue-loader',
-				},
-				{
-					test: /\.js?$/,
-					exclude: /node_modules/,
-					loader: 'babel-loader',
-				},
-				{
-					test: /\.css$/,
-					use: ['style-loader', 'css-loader', 'sass-loader'],
-				},
-				{
-					test: /\.scss$/,
-					use: ['style-loader', 'css-loader', 'sass-loader'],
-				},
-			],
-		},
-		plugins: [new vueLoader.VueLoaderPlugin()],
-	},
-	logger: {
-		info: console.log,
-		warn: message => console.warn(`Warning: ${message}`),
-	},
-	serverPort: 8082,
-}).server((err, config) => {
+config.logger = {
+	info: console.log,
+	warn: message => console.warn(`Warning: ${message}`),
+};
+
+config.serverPort = 8082;
+config.components = path.resolve(dir, 'src/components/**/[A-Z]*.vue');
+
+styleguidist(config).server((err, config) => {
 	if (err) {
 		console.log(err);
 	} else {
