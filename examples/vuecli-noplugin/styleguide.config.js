35e28272e13939a640c33d9ef14f48bd9a05c489
Barthelemy Ledoux <bart.ledoux@enablon.com>
2019-01-29 08:33:13 -0600

    add example of @vue/cli noplugin

---
+++ examples/vuecli-noplugin/styleguide.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	// set your styleguidist configuration here
+	title: 'Default Style Guide',
+	defaultExample: true,
+	components: 'src/components/**/[A-Z]*.vue',
+	// sections: [
+	//   {
+	//     name: 'First Section',
+	//     components: 'src/components/**/[A-Z]*.vue'
+	//   }
+	// ],
+	// webpackConfig: {
+	//   // custom config goes here
+	// }
+};
