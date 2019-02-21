3f23c315b0540d731be7843a61abad71b6377f7a
Barthélémy Ledoux <ledouxb@me.com>
2019-02-21 09:57:59 -0600

    docs: add meta for opengraph sharing and SEO (#288)

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,25 @@
+const title = 'Vue Styleguidist';
+const description = 'Isolated Vue component development environment with a living style guide';
+
 module.exports = {
 	dest: 'docs/dist',
-	title: 'Vue Styleguidist',
-	description: 'Isolated Vue component development environment with a living style guide',
+	title,
+	description,
+	head: [
+		['meta', { prefix: ogprefix, property: 'og:title', content: title }],
+		['meta', { prefix: ogprefix, property: 'twitter:title', content: title }],
+		['meta', { prefix: ogprefix, property: 'og:type', content: 'website' }],
+		['meta', { prefix: ogprefix, property: 'og:description', content: description }],
+		[
+			'meta',
+			{
+				prefix: ogprefix,
+				property: 'og:image',
+				content:
+					'https://raw.githubusercontent.com/vue-styleguidist/vue-styleguidist/master/assets/logo.png',
+			},
+		],
+	],
 	themeConfig: {
 		repo: 'vue-styleguidist/vue-styleguidist',
 		editLinks: true,
