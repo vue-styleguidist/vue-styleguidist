b132cad8675428b0937ea3401445c9fda6be218e
Bart Ledoux <ledouxb@me.com>
2019-02-28 09:42:31 -0600

    fix tests

--- packages/vue-docgen-api/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	transform: {
-		'^.+\\.ts$': 'ts-jest',
-	},
-	moduleFileExtensions: ['ts', 'js'],
-	testMatch: ['<rootDir>/tests/**/*.test.(ts|js)', '<rootDir>/src/**/__tests__/**/*.(ts|js)'],
-};
