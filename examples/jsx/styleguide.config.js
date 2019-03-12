878868b14b0826989f2fd374408a348bddeea09c
elevatebart <ledouxb@me.com>
2019-03-11 22:13:09 -0500

    test(jsx): add first integration test for jsx

---
+++ examples/jsx/styleguide.config.js
@@ -0,0 +1,34 @@
+const vueLoader = require('vue-loader')
+
+module.exports = {
+	title: 'Vue Styleguidist basic',
+	components: 'src/components/**/[A-Z]*.jsx',
+	defaultExample: true,
+	ribbon: {
+		url: 'https://github.com/vue-styleguidist/vue-styleguidist'
+	},
+	version: '1.1.1',
+	webpackConfig: {
+		module: {
+			rules: [
+				{
+					test: /\.vue$/,
+					loader: 'vue-loader'
+				},
+				{
+					test: /\.js?$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader'
+				},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader']
+				}
+			]
+		},
+
+		plugins: [new vueLoader.VueLoaderPlugin()]
+	},
+	usageMode: 'expand',
+	exampleMode: 'expand'
+}
