3eb77f4bb19d2cc810a026b95d762cac0a299f27
Barthélémy Ledoux <ledouxb@me.com>
2019-03-12 19:03:19 -0500

    test(nuxtjs): implement axios modules (#317)
    In order to ilustrate how to mock module usage in component showcase for nuxtjs, I created a new component in the nuxt example that uses `this.$axios.$get()`. I mocked the `$get` function.
    
    closes #244

--- examples/nuxtjs/babel.config.js
+++ examples/nuxtjs/babel.config.js
@@ -8,6 +8,7 @@ module.exports = {
 		]
 	],
 	plugins: [
+		'@babel/plugin-transform-runtime',
 		'@babel/plugin-syntax-dynamic-import',
 		'@babel/plugin-proposal-class-properties',
 		'@babel/plugin-proposal-object-rest-spread'
--- examples/nuxtjs/styleguide/styleguide.root.js
+++ examples/nuxtjs/styleguide/styleguide.root.js
@@ -4,6 +4,12 @@ import { state, mutations, getters } from "../src/store/mutations";
 
 Vue.use(Vuex);
 
+// in order not to polute the database and making useless database calls
+// we mock axios (we could have used mock-axios instead)
+Vue.prototype.$axios = {
+  $get:() => Promise.resolve('success') 
+};
+
 const store = new Vuex.Store({
   state,
   getters,
