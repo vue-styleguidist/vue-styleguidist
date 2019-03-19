5a2f9b21ceecf0068c014b30c8b32da5ee625180
Bart Ledoux <ledouxb@me.com>
2019-03-19 18:56:16 -0500

    docs(loaders): add some todos

--- packages/vue-styleguidist/loaders/examples-loader.js
+++ packages/vue-styleguidist/loaders/examples-loader.js
@@ -8,7 +8,13 @@ function isVueFile(filepath) {
 module.exports = function examplesLoader(source) {
 	const file = this.request.split('!').pop()
 	if (isVueFile(file)) {
+		// if it's a vue file, the examples are in a docs block
 		source = getComponentVueDoc(source, file)
+	} else {
+		// TODO: extract all <script> tags from vue examples
+		// in each <script> tag, extract require and imports and start with it
+		// acornJsx in https://github.com/styleguidist/react-styleguidist/blob/master/src/loaders/utils/getImports.js
+		// does not understand vue syntax
 	}
 	return examplesLoaderRsg.call(this, source)
 }
