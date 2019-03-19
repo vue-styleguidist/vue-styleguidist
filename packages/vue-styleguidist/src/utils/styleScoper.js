7fc1ad2dffc5a853ec3a196e4453e9c69c309f88
elevatebart <ledouxb@me.com>
2019-03-18 22:28:17 -0500

    fix(styleguide): fix scoped pseudo selectors
    Closes #335

--- packages/vue-styleguidist/src/utils/styleScoper.js
+++ packages/vue-styleguidist/src/utils/styleScoper.js
@@ -8,24 +8,38 @@ function init() {
 	style.sheet.insertRule('body { visibility: hidden; }', 0)
 }
 
-function scoper(css, suffix) {
-	const re = new RegExp('([^\r\n,{}]+)(,(?=[^}]*{)|s*{)', 'g')
-	css = css.replace(re, function(g0, g1, g2) {
-		if (g1.match(/^\s*(@media|@keyframes|to|from|@font-face)/)) {
-			return g1 + g2
+export function scoper(css, suffix) {
+	const re = /([^\r\n,{}]+)(,(?=[^}]*{)|s*{)/g
+
+	// `after` is going to contain eithe a comma or an opening curly bracket
+	css = css.replace(re, function(full, selector, after) {
+		// if non-rule delimiter
+		if (selector.match(/^\s*(@media|@keyframes|to|from|@font-face)/)) {
+			return selector + after
 		}
 
-		if (g1.match(/:scope/)) {
-			g1 = g1.replace(/([^\s]*):scope/, function(h0, h1) {
-				if (h1 === '') {
+		// deal with :scope pseudo selectors
+		if (selector.match(/:scope/)) {
+			selector = selector.replace(/([^\s]*):scope/, function(full, cutSelector) {
+				if (cutSelector === '') {
 					return '> *'
 				}
-				return '> ' + h1
+				return '> ' + cutSelector
 			})
 		}
-		g1 = g1.trim() + ' '
-		g1 = g1.replace(/ /g, suffix + ' ')
-		return g1 + g2
+
+		// deal with other pseudo selectors
+		let pseudo = ''
+		if (selector.match(/:/)) {
+			const parts = selector.split(/:/)
+			selector = parts[0]
+			pseudo = ':' + parts[1]
+		}
+
+		selector = selector.trim() + ' '
+		selector = selector.replace(/ /g, suffix + pseudo + ' ')
+
+		return selector + after
 	})
 
 	return css
