2c66c05f20475b596ae73fb4dc0cd3f287200796
elevatebart <ledouxb@me.com>
2019-03-27 23:20:13 +0100

    docs(examples): add functional example to the basic styleguide

---
+++ examples/basic/src/components/Functional/FunctionalButton.vue
@@ -0,0 +1,15 @@
+<template functional>
+  <!-- @prop {string} id name attribute of the button -->
+  <button :name="props.id" style="background:pink">
+    <!-- @slot name of the button -->
+    <slot/>
+  </button>
+</template>
+
+<docs>
+```vue
+<functional-button id="test">
+    hello
+</functional-button>
+```
+</docs>
