f67665fc015dd68f6fdfed2796d5f1f3c1cd95ff
Bart Ledoux <ledouxb@me.com>
2019-04-02 05:04:45 -0500

    docs: fix nuxtjs example

--- examples/nuxtjs/styleguide/styleguide.root.js
+++ examples/nuxtjs/styleguide/styleguide.root.js
@@ -7,7 +7,14 @@ Vue.use(Vuex);
 // in order not to polute the database and making useless database calls
 // we mock axios (we could have used mock-axios instead)
 Vue.prototype.$axios = {
-  $get:() => Promise.resolve('success') 
+  $get:() => Promise.resolve([
+    {employee_name:'success', employee_salary: "12"}, 
+    {employee_name:'joe', employee_salary: "1789"},
+    {employee_name:'bill', employee_salary: "13"},
+    {employee_name:'martin', employee_salary: "1234"},
+    {employee_name:'anette', employee_salary: "34"},
+    {employee_name:'hilary', employee_salary: "567"},
+  ]) 
 };
 
 const store = new Vuex.Store({
