41a4855ada828e8a231624b20cf49b4a1ae3df66
elevatebart <ledouxb@me.com>
2019-04-28 16:38:21 -0500

    docs: add svg-loader example

---
+++ examples/svg-loader/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+	name: 'app'
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
---
+++ examples/svg-loader/vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+	chainWebpack: config => {
+		const svgRule = config.module.rule('svg')
+
+		svgRule.uses.clear()
+
+		svgRule.use('vue-svg-loader').loader('vue-svg-loader')
+	}
+}
