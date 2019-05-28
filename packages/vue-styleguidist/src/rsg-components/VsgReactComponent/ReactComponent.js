17b335f22b89a3ee51d3bcfb6a6e7f34e0490a01
elevatebart <ledouxb@me.com>
2019-05-28 11:22:37 -0500

    refactor: cirecular reference blocking build

--- packages/vue-styleguidist/src/rsg-components/VsgReactComponent/ReactComponent.js
+++ packages/vue-styleguidist/src/rsg-components/VsgReactComponent/ReactComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import RsgReactComponent from 'rsg-components/ReactComponent'
+import RsgReactComponent from 'rsg-components/ReactComponent/ReactComponent'
 
 export const DocumentedComponentContext = React.createContext({})
 
