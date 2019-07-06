430b3604334761cd86d787294138583e0f170f83
elevatebart <ledouxb@me.com>
2019-07-06 14:15:44 -0500

    test: add lerna test

---
+++ packages/vue-docgen-api/tests/components/mixin-resolved-lerna/button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button
+    class="buttonComponent"
+    @click.prevent="onClick"
+    :style="`background:${color};width:${size}px;`"
+  />
+</template>
+
+<script>
+import myMixin from '@test-vue-docgen/mixins'
+
+export default {
+	mixins: [myMixin]
+}
+</script>
