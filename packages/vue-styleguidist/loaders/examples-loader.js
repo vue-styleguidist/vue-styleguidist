a6db6cbafcbb8ce7614257a57345f9d96ac073fe
elevatebart <ledouxb@me.com>
2019-07-13 17:27:00 -0500

    feat: use the JSX capabilities of compiler

--- packages/vue-styleguidist/loaders/examples-loader.js
+++ packages/vue-styleguidist/loaders/examples-loader.js
@@ -19,13 +19,14 @@ const cleanComponentName = require('./utils/cleanComponentName')
 // Hack the react scaffolding to be able to load client
 const absolutize = filepath =>
 	path.resolve(
-		path.dirname(require.resolve('react-styleguidist')),
+		path.dirname(require.resolve('vue-styleguidist')),
 		'../loaders/utils/client',
 		filepath
 	)
 
 const REQUIRE_IN_RUNTIME_PATH = absolutize('requireInRuntime')
 const EVAL_IN_CONTEXT_PATH = absolutize('evalInContext')
+const PRAGMA_JSX_PATH = require.resolve('vue-inbrowser-compiler')
 
 function isVueFile(filepath) {
 	return /.vue$/.test(filepath)
@@ -38,7 +39,8 @@ module.exports = function examplesLoader(source) {
 		source = getComponentVueDoc(source, filePath)
 	}
 	const config = this._styleguidist
-	const { displayName, shouldShowDefaultExample, customLangs } = loaderUtils.getOptions(this) || {}
+	const { file, displayName, shouldShowDefaultExample, customLangs } =
+		loaderUtils.getOptions(this) || {}
 
 	const cleanDisplayName = displayName ? cleanComponentName(displayName) : undefined
 	// Replace placeholders (__COMPONENT__) with the passed-in component name
@@ -54,6 +56,11 @@ module.exports = function examplesLoader(source) {
 	const examples = chunkify(source, updateExample, customLangs)
 
 	const getScript = code => {
+		// if in JSX mode just parse code as is
+		if (config.jsxInExamples) {
+			return code
+		}
+
 		// script is at the beginning of a line after a return
 		// In case we are loading a vue component as an example, extract script tag
 		if (isCodeVueSfc(code)) {
@@ -64,9 +71,7 @@ module.exports = function examplesLoader(source) {
 		return code.split(/\n[\t ]*</)[0]
 	}
 
-	const getExampleLiveImports = source => {
-		return getImports(getScript(source))
-	}
+	const getExampleLiveImports = source => getImports(getScript(source))
 
 	// Find all import statements and require() calls in examples to make them
 	// available in webpack context at runtime.
@@ -77,8 +82,21 @@ module.exports = function examplesLoader(source) {
 		return requires.concat(getExampleLiveImports(example.content))
 	}, [])
 
+	// Auto imported modules.
+	// We don't need to do anything here to support explicit imports: they will
+	// work because both imports (generated below and by rewrite-imports) will
+	// be eventually transpiled to `var x = require('x')`, so we'll just have two
+	// of them in the same scope, which is fine in non-strict mode
+	const fullContext = {
+		// Modules, provied by the user
+		...config.context,
+		// Append the current component module to make it accessible in examples
+		// without an explicit import
+		...(displayName && config.jsxInExamples ? { [displayName]: file } : {})
+	}
+
 	// All required or imported modules
-	const allModules = [...requiresFromExamples, ...values(config.context)]
+	const allModules = [...requiresFromExamples, ...values(fullContext)]
 
 	// “Prerequire” modules required in Markdown examples and context so they
 	// end up in a bundle and be available at runtime
@@ -88,18 +106,26 @@ module.exports = function examplesLoader(source) {
 	}, {})
 
 	// Require context modules so they are available in an example
+	let marker = -1
 	const requireContextCode = b.program(
 		flatten(
-			map(config.context, (requireRequest, name) => [
+			map(fullContext, (requireRequest, name) => [
 				// const name$0 = require(path);
 				b.variableDeclaration('const', [
-					b.variableDeclarator(b.identifier(`${name}$0`), requireIt(requireRequest).toAST())
+					b.variableDeclarator(
+						b.identifier(`${name}$${++marker}`),
+						requireIt(requireRequest).toAST()
+					)
 				]),
 				// const name = name$0.default || name$0;
 				b.variableDeclaration('const', [
 					b.variableDeclarator(
 						b.identifier(name),
-						b.logicalExpression('||', b.identifier(`${name}$0.default`), b.identifier(`${name}$0`))
+						b.logicalExpression(
+							'||',
+							b.identifier(`${name}$${marker}.default`),
+							b.identifier(`${name}$${marker}`)
+						)
 					)
 				])
 			])
@@ -119,12 +145,14 @@ if (module.hot) {
 	module.hot.accept([])
 }
 var requireMap = ${generate(toAst(allModulesCode))};
-var requireInRuntimeBase = require(${JSON.stringify(REQUIRE_IN_RUNTIME_PATH)}).default;
+var requireInRuntimeBase = require(${JSON.stringify(REQUIRE_IN_RUNTIME_PATH)});
 var requireInRuntime = requireInRuntimeBase.bind(null, requireMap);
-var evalInContextBase = require(${JSON.stringify(EVAL_IN_CONTEXT_PATH)}).default;
+var __pragma__ = require(${JSON.stringify(PRAGMA_JSX_PATH)}).pragmaJSX;
+
+var evalInContextBase = require(${JSON.stringify(EVAL_IN_CONTEXT_PATH)});
 var evalInContext = evalInContextBase.bind(null, ${JSON.stringify(
 		generate(requireContextCode)
-	)}, requireInRuntime);
+	)}, requireInRuntime, __pragma__);
 module.exports = ${generate(toAst(examplesWithEval))}
 	`
 }
