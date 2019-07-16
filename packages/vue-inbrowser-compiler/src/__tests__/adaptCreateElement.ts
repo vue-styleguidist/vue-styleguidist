be6de16485a159c9e93b80713ec2c5ca1fe32fce
elevatebart <ledouxb@me.com>
2019-07-16 08:00:48 -0500

    fix(compiler): add normal attributes in attrs

---
+++ packages/vue-inbrowser-compiler/src/__tests__/adaptCreateElement.ts
@@ -0,0 +1,38 @@
+import adaptCreateElement from '../adaptCreateElement'
+
+type ElCreator = (co: string | object, at?: { [k: string]: any }, ch?: any | any[]) => void
+
+describe('adaptCreateElement', () => {
+	let h: jest.Mock<ElCreator>
+	let pragma: ElCreator
+	beforeEach(() => {
+		h = jest.fn()
+		pragma = adaptCreateElement(h)
+	})
+
+	it('should merge all non special attributes in attrs', () => {
+		pragma('Component', { hello: 'hello' })
+		expect(h).toHaveBeenCalledWith('Component', { attrs: { hello: 'hello' } })
+	})
+
+	it('should set all on attributes in an on object', () => {
+		const handler = jest.fn()
+		pragma('Component', { onClick: handler })
+		expect(h).toHaveBeenCalledWith('Component', { on: { click: handler } })
+	})
+
+	it('should set all nativeOn attributes in a nativeOn object', () => {
+		const handler = jest.fn()
+		pragma('Component', { nativeOnClick: handler })
+		expect(h).toHaveBeenCalledWith('Component', { nativeOn: { click: handler } })
+	})
+
+	it('should transform kebab-case into camel-case', () => {
+		const handler = jest.fn()
+		pragma('Component', { 'native-on-click': handler, 'hello-world': 'foo' })
+		expect(h).toHaveBeenCalledWith('Component', {
+			nativeOn: { click: handler },
+			attrs: { helloWorld: 'foo' }
+		})
+	})
+})
