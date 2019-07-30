7d6d78518c6c64ab472c3253ab04dea6d257ead5
Bart Ledoux <ledouxb@me.com>
2019-07-30 17:04:28 -0500

    ci: use cypress docker image
    in order to avoid having to install cypress we use a pre-installed
    cypress docker image

--- test/run.server.js
+++ test/run.server.js
@@ -1,11 +1,9 @@
 const path = require('path')
-// eslint-disable-next-line import/no-unresolved
-const cypress = require('cypress')
 const styleguidist = require('../packages/vue-styleguidist/scripts')
 
 /* eslint-disable no-console */
 
-const exampleName = process.argv[2] || 'basic'
+const exampleName = process.env.CY_EXAMPLE_FOLDER || 'basic'
 
 const dir = path.resolve(__dirname, '../examples', exampleName)
 const config = require(path.join(dir, 'styleguide.config'))
@@ -20,20 +18,6 @@ config.components = path.resolve(dir, 'src/components/**/[A-Z]*.{vue,jsx}')
 delete config.ribbon
 delete config.usageMode
 delete config.exampleMode
+delete config.codeSplit
 
-const { app } = styleguidist(config).server(err => {
-	if (err) {
-		throw err
-	} else {
-		cypress
-			.run({
-				spec: [path.join(__dirname, 'cypress/integration', exampleName, '*_spec.js')]
-			})
-			.then(() => {
-				app.close()
-			})
-			.catch(err => {
-				throw err
-			})
-	}
-})
+styleguidist(config).server(() => {})
