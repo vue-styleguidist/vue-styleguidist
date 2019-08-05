ef41b8bd211eb6681327d8870533d32727813376
Bart Ledoux <ledouxb@me.com>
2019-08-05 13:53:59 -0500

    docs: update vuetify example for compat with 2.0

--- examples/vuetify/config/styleguide.root.js
+++ examples/vuetify/config/styleguide.root.js
@@ -1,4 +1,5 @@
 import VueI18n from 'vue-i18n'
+import Vuetify from 'vuetify'
 import messages from './i18n'
 import Languages from './Languages.vue'
 
@@ -10,6 +11,7 @@ const i18n = new VueI18n({
 export default previewComponent => {
 	// https://vuejs.org/v2/guide/render-function.html
 	return {
+		vuetify: new Vuetify(),
 		i18n,
 		render(createElement) {
 			return createElement(
--- examples/vuetify/styleguide.config.js
+++ examples/vuetify/styleguide.config.js
@@ -33,7 +33,7 @@ module.exports = {
 				},
 				{
 					test: /\.css$/,
-					use: ['style-loader', 'css-loader', 'sass-loader']
+					use: ['style-loader', 'css-loader']
 				},
 				{
 					test: /\.scss$/,
