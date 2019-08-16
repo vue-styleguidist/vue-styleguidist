7447b50596085eb596a8bc93170ab8d924485932
elevatebart <ledouxb@me.com>
2019-08-16 14:35:42 -0500

    docs: examples

--- test/run.cli.js
+++ test/run.cli.js
@@ -1,13 +1,20 @@
-var exampleName = process.argv[2] || 'basic'
+const args = require('minimist')(process.argv.slice(2))
 
-if (/^--/.test(exampleName)) {
-	exampleName = 'basic'
+if (!/examples[\\/][^\\/]+$/.test(process.cwd())) {
+	process.chdir(args._[0])
 }
 
-const examplePath = require('path').join('examples', exampleName)
-
-process.chdir(examplePath)
-
-process.argv[2] = require('minimist')(process.argv.slice(2)).watch ? '--watch' : undefined
+process.argv[2] = args.watch ? '--watch' : undefined
 
 require('../packages/vue-docgen-cli/lib/bin.js')
+
+if (args._.includes('build')) {
+	process.argv[2] = 'build'
+} else {
+	process.argv[2] = 'dev'
+}
+
+if (args.watch) {
+	process.argv[3] = 'docs'
+	require('vuepress/cli')
+}
--- test/styleguidist.js
+++ test/styleguidist.js
@@ -19,6 +19,10 @@ if (examplePath === 'examples/vuecli3' || examplePath === 'examples/svg-loader')
 	const command = process.argv[2] === 'server' ? '' : `:${process.argv[2]}`
 	process.argv[2] = `styleguidist${command}`
 	require('@vue/cli-service/bin/vue-cli-service')
+} else if (/examples\/docgen/.test(examplePath)) {
+	process.argv[3] = process.argv[2]
+	process.argv[2] = examplePath
+	require('./run.cli')
 } else {
 	require('../packages/vue-styleguidist/bin/styleguidist')
 }
