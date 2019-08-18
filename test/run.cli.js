7935956ea1f15ade807d8dd3b9a8321095e15b6b
elevatebart <ledouxb@me.com>
2019-08-18 17:37:31 -0500

    fix(cli): take cwd param into account

--- test/run.cli.js
+++ test/run.cli.js
@@ -8,17 +8,14 @@ if (!exampleName) {
 	exampleName = 'basic'
 }
 
-const examplePath = path.join('examples', exampleName)
-if (!command) {
-	command = 'dev'
-}
+const examplePath = path.join(__dirname, '..', 'examples', exampleName)
 process.argv[2] = command === 'dev' ? '--watch' : undefined
 process.argv[3] = '--cwd'
 process.argv[4] = examplePath
 
 require('../packages/vue-docgen-cli/lib/bin.js')
 
-if (fs.existsSync(path.join(examplePath, 'docs', '.vuepress', 'config.js'))) {
+if (command && fs.existsSync(path.join(examplePath, 'docs', '.vuepress', 'config.js'))) {
 	process.argv[2] = command
 	process.argv[3] = path.join(examplePath, 'docs')
 	require('vuepress/cli')
