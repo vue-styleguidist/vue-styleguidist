648a6b2a133d29361ad9a2e8813fc3cb4f846771
Bart Ledoux <ledouxb@me.com>
2019-12-18 14:43:28 -0600

    test: add babel bug test

--- packages/vue-docgen-api/src/script-handlers/__tests__/componentHandler.ts
+++ packages/vue-docgen-api/src/script-handlers/__tests__/componentHandler.ts
@@ -23,7 +23,7 @@ describe('componentHandler', () => {
 	it('should return the right component name', () => {
 		const src = `
     /**
-     * An empty component
+     * An empty component #1
      */
     export default {
       name: 'name-123',
@@ -33,13 +33,31 @@ describe('componentHandler', () => {
 		if (def) {
 			componentHandler(documentation, def)
 		}
-		expect(documentation.set).toHaveBeenCalledWith('description', 'An empty component')
+		expect(documentation.set).toHaveBeenCalledWith('description', 'An empty component #1')
+	})
+
+	it('should return the right component description with import', () => {
+		const src = `
+	import { test } from 'vue-test'
+		
+    /**
+     * An empty component #1.1
+     */
+    export default {
+      name: 'name-123',
+    }
+    `
+		const def = parse(src).get('default')
+		if (def) {
+			componentHandler(documentation, def)
+		}
+		expect(documentation.set).toHaveBeenCalledWith('description', 'An empty component #1.1')
 	})
 
 	it('should return tags for normal components', () => {
 		const src = `
     /**
-     * An empty component
+     * An empty component #2
      * @version 12.5.7
      * @author [Rafael]
      */
@@ -60,7 +78,7 @@ describe('componentHandler', () => {
 	it('should return tags for class style components', () => {
 		const src = `
     /**
-     * An empty component
+     * An empty component #3
      * @version 12.5.7
      */
     @Component
