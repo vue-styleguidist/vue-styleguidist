88af49c7700f99646e7c79990459e9b7d7bfb5f4
Bart Ledoux <ledouxb@me.com>
2020-01-17 17:28:37 -0600

    style: add comment

--- packages/vue-docgen-api/src/utils/documentRequiredComponents.ts
+++ packages/vue-docgen-api/src/utils/documentRequiredComponents.ts
@@ -8,6 +8,7 @@ import makePathResolver from './makePathResolver'
 
 /**
  * Document all components in varToFilePath in documentation
+ * Instead of giving it only one component file, here we give it a whole set of variable -> file
  *
  * @param documentation if omitted (undefined), it will return all docs in an array
  * @param varToFilePath variable of object to document
