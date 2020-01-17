db301fa37da9b9c06ba07b861ce2c2548d035c69
elevatebart <ledouxb@me.com>
2020-01-17 08:36:21 -0600

    test: sfc multiple exports

---
+++ packages/vue-docgen-api/tests/components/multiple-exports/MyButton.vue
@@ -0,0 +1,110 @@
+<script>
+import extendMixin from '../../mixins/extendMixin'
+
+export const name = 'EverButton'
+
+/**
+ * This is a button that represents a javascript only component, not a vue SFC
+ */
+export const Button = {
+	name,
+	mixins: [extendMixin],
+	inheritAttrs: false,
+
+	props: {
+		as: {
+			type: String,
+			default: 'button'
+		},
+		type: {
+			type: String,
+			default: 'button'
+		},
+		variant: {
+			type: String,
+			default: 'solid'
+		},
+		variantColor: {
+			type: String,
+			default: 'gray'
+		},
+		size: {
+			type: [String, Number],
+			default: 'md'
+		},
+		isDisabled: {
+			type: Boolean
+		}
+	},
+
+	render(h) {
+		const childAttrs = {
+			as: this.as,
+			disabled: this.isDisabled,
+			'aria-disabled': this.isDisabled
+		}
+
+		if (this.as === 'button') {
+			childAttrs.type = this.type
+		} else {
+			childAttrs.role = 'button'
+		}
+
+		return h(
+			'div',
+			{
+				attrs: childAttrs,
+				on: this.$listeners
+			},
+			this.$slots.default
+		)
+	}
+}
+
+/**
+ * This is an input that represents another component extracted in the same file
+ */
+export const Input = {
+	name,
+
+	inheritAttrs: false,
+
+	props: {
+		variant: {
+			type: String,
+			default: 'solid'
+		},
+		variantColor: {
+			type: String,
+			default: 'gray'
+		},
+		size: {
+			type: [String, Number],
+			default: 'md'
+		},
+		isDisabled: {
+			type: Boolean
+		}
+	},
+
+	render(h) {
+		const childAttrs = {
+			disabled: this.isDisabled,
+			'aria-disabled': this.isDisabled
+		}
+
+		return h(
+			'input',
+			{
+				attrs: childAttrs,
+				on: this.$listeners
+			},
+			this.$slots.default
+		)
+	}
+}
+</script>
+
+<template>
+	<div />
+</template>
---
+++ packages/vue-docgen-api/tests/components/multiple-exports/multiple-exports.test.ts
@@ -0,0 +1,52 @@
+import * as path from 'path'
+import { ComponentDoc } from '../../../src/Documentation'
+import { parseMulti } from '../../../src/main'
+
+const button = path.join(__dirname, './MyButton.vue')
+let docButton: ComponentDoc[]
+describe('tests components with multiple exports', () => {
+	beforeAll(async done => {
+		docButton = await parseMulti(button)
+		done()
+	})
+
+	it('should extract the export name', () => {
+		expect(docButton[0].exportName).toBe('Button')
+	})
+
+	it('should have the correct content in the extracted definition', () => {
+		expect(docButton[0].description).toBe(
+			'This is a button that represents a javascript only component, not a vue SFC'
+		)
+	})
+
+	it('should export mixins prop', () => {
+		expect(docButton[0].props && docButton[0].props.map(p => p.name)).toMatchInlineSnapshot(`
+		Array [
+		  "color",
+		  "id",
+		  "v-model",
+		  "falseValue",
+		  "trueValue",
+		  "multiple",
+		  "label",
+		  "as",
+		  "type",
+		  "variant",
+		  "variantColor",
+		  "size",
+		  "isDisabled",
+		]
+	`)
+	})
+
+	it('should extract the export name for input', () => {
+		expect(docButton[1].exportName).toBe('Input')
+	})
+
+	it('should have contain the input definition', () => {
+		expect(docButton[1].description).toBe(
+			'This is an input that represents another component extracted in the same file'
+		)
+	})
+})
