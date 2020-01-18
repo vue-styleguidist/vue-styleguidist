7725ed87c7d84ed26c65abfc00674a9ef80d38fc
Bart Ledoux <ledouxb@me.com>
2020-01-17 18:18:37 -0600

    docs: make examples for hmr theme

---
+++ examples/customised/styleguide/styles.js
@@ -0,0 +1,19 @@
+export default {
+	Playground: {
+		preview: {
+			paddingLeft: 0,
+			paddingRight: 0,
+			borderWidth: [[0, 0, 1, 0]],
+			borderRadius: 0
+		}
+	},
+	Markdown: {
+		pre: {
+			border: 0,
+			background: 'none'
+		},
+		code: {
+			fontSize: 14
+		}
+	}
+}
