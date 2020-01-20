d4903b581fa4abe6a315d178c127de4ba1604acd
Bart Ledoux <ledouxb@me.com>
2020-01-20 15:00:49 -0600

    docs: remove warnings in custom example

--- examples/vuecli3-custom/src/App.vue
+++ examples/vuecli3-custom/src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<img src="./assets/logo.png" />
+		<HelloWorld msg="Welcome to Your Vue.js App" />
+	</div>
 </template>
 
 <script>
