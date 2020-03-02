deb2dc7ebda80938d59ab458faa8699f7305eb35
elevatebart <ledouxb@me.com>
2020-03-02 11:38:40 -0600

    fix: an SFC example can contain JSX

--- packages/vue-styleguidist/src/loaders/utils/getScript.ts
+++ packages/vue-styleguidist/src/loaders/utils/getScript.ts
@@ -2,16 +2,15 @@ import { parseComponent } from 'vue-template-compiler'
 import { isCodeVueSfc } from 'vue-inbrowser-compiler-utils'
 
 export default (code: string, jsxInExamples: boolean): string => {
-	// if in JSX mode or litteral return examples code as is
-	if (jsxInExamples || /new Vue\(/.test(code)) {
-		return code
-	}
-
-	// script is at the beginning of a line after a return
 	// In case we are loading a vue component as an example, extract script tag
 	if (isCodeVueSfc(code)) {
 		const parts = parseComponent(code)
 		return parts && parts.script ? parts.script.content : ''
+	}
+
+	// if in JSX mode or litteral return examples code as is
+	if (jsxInExamples || /new Vue\(/.test(code)) {
+		return code
 	}
 
 	if (/\n\W+?export\W+default\W/.test(code) || /\n\W+?module.exports(\W+)?=/.test(code)) {
