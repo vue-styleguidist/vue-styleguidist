80f84d01204bb90e76ae353260cb205b295b663d
Özüm Eldoğan <ozum@ozum.net>
2020-03-06 11:39:25 +0300

    docs: ✏️ add nuxtjs example with TS and property-decorator
    Add nuxtjs example using class syntax with TypeScript and
    nuxt-propert-decorator package
    
    ✅ Closes: #783

---
+++ examples/nuxtjs-ts-property-decorator/nuxt.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+	srcDir: 'src/',
+	/*
+  ** Headers of the page
+  */
+	head: {
+		title: 'styleguide',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: 'Nuxt.js project' }
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+	},
+	/*
+  ** Customize the progress bar color
+  */
+	loading: { color: '#3B8070' },
+	modules: ['@nuxtjs/axios']
+}
---
+++ examples/nuxtjs-ts-property-decorator/src/components/SomeComponent.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		HELLO
+	</div>
+</template>
+
+<script lang="ts">
+
+import { Vue, Component, Prop } from 'nuxt-property-decorator'
+
+@Component
+export default class SomeComponent extends Vue {
+	/** Description for propA */
+	@Prop({ type: Number, default: 3}) readonly propA!: number
+
+  /**
+   * Description for mwthod. Don't froget to put `@public`.
+	 * @public
+   */
+	public onClick(name: string) {
+    /**
+     * Success event when we click.
+		 * @property {string} name description of name.
+     * @property {boolean} success always true.
+     */
+    this.$emit('success', { name, success: true })
+	}
+}
+</script>
