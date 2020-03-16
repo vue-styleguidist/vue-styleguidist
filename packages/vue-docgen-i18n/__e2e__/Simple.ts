881e128773f6f396ffc8ad6c6499609d92299345
elevatebart <ledouxb@me.com>
2020-03-16 09:26:50 -0500

    test(i18n): move tests in e2e

--- packages/vue-docgen-i18n/__e2e__/Simple.ts
+++ packages/vue-docgen-i18n/__e2e__/Simple.ts
@@ -4,11 +4,11 @@ import execa from 'execa'
 
 describe('Simple', () => {
 	beforeAll(async done => {
-		await execa('vue-docgen-i18n', ['./Button.vue', '--lang', 'FR'], { cwd: __dirname })
+		await execa('vue-docgen-i18n', ['./__mocks__/Button.vue', '--lang', 'FR'], { cwd: __dirname })
 		done()
 	})
 
 	it('should generate a translation file', () => {
-		expect(fs.existsSync(path.join(__dirname, './Button.FR.js'))).toBeTruthy()
+		expect(fs.existsSync(path.join(__dirname, './__mocks__/Button.FR.js'))).toBeTruthy()
 	})
 })
