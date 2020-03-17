ccd52d83ed7fafa6faf2f7ad9e31ff7ef0fd375f
elevatebart <ledouxb@me.com>
2020-03-17 13:47:36 -0500

    fix: make interface for codetabbutton exported

--- packages/vue-styleguidist/src/client/rsg-components/slots/CodeTabButton.tsx
+++ packages/vue-styleguidist/src/client/rsg-components/slots/CodeTabButton.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import TabButton from 'rsg-components/TabButton'
 
-interface CodeTabeButtonProps {
+export interface CodeTabButtonProps {
 	onClick: (e: React.MouseEvent) => void
 	name: string
 	active?: boolean
 }
 
-const CodeTabButton = (props: CodeTabeButtonProps) => <TabButton {...props}>View Code</TabButton>
+const CodeTabButton = (props: CodeTabButtonProps) => <TabButton {...props}>View Code</TabButton>
 
 CodeTabButton.propTypes = {
 	onClick: PropTypes.func.isRequired,
