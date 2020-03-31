f5bbc416e108db1b313d4a2c55c8e1bc845d03ca
Barthélémy Ledoux <bledoux@shipbob.com>
2020-03-31 09:54:25 -0500

    fix: allow ignoreWithoutExamples to work with docs

--- packages/vue-styleguidist/src/loaders/utils/processComponent.ts
+++ packages/vue-styleguidist/src/loaders/utils/processComponent.ts
@@ -38,8 +38,8 @@ export default function processComponent(
 	const componentMetadataPath = getComponentMetadataPath(filepath)
 	const hasExamplesFile = examplesFile && fs.existsSync(examplesFile)
 	let hasInternalExamples = false
-	if (!hasExamplesFile && fs.existsSync(componentPath)) {
-		const customBlocks = parseComponent(fs.readFileSync(componentPath, 'utf8')).customBlocks
+	if (!hasExamplesFile && fs.existsSync(filepath)) {
+		const customBlocks = parseComponent(fs.readFileSync(filepath, 'utf8')).customBlocks
 		hasInternalExamples = !!customBlocks && customBlocks.findIndex(p => p.type === 'docs') >= 0
 	}
 	const hasExamples = hasExamplesFile || hasInternalExamples
