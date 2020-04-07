2494a6d339b7ce2a30b768f144a5ca666bc745d4
elevatebart <ledouxb@me.com>
2020-04-07 09:27:33 -0500

    docs: add example dropdown meta component

---
+++ examples/basic/src/components/DropDown/DropDown.vue
@@ -0,0 +1,21 @@
+<template>
+  <select name="mySelect">
+    <slot />
+  </select>
+</template>
+<script>
+/**
+ * @requires ./Option.vue
+ */
+export default {
+	name: 'DropDown'
+}
+</script>
+<docs>
+```jsx
+<DropDown>
+    <Option val="1">test</Option>
+    <Option val="1">other</Option>
+</DropDown>
+```
+</docs>
---
+++ examples/basic/src/components/DropDown/Option.vue
@@ -0,0 +1,16 @@
+<template>
+  <option :value="val">
+    <slot />
+  </option>
+</template>
+<script>
+export default {
+	name: 'Option',
+	props: {
+		val: {
+			type: String,
+			required: true
+		}
+	}
+}
+</script>
