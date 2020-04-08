caa605e45c3b2afccc19950b98cd97cbd8259507
elevatebart <ledouxb@me.com>
2020-04-08 14:59:27 -0500

    style: react component is now ts

--- packages/vue-styleguidist/src/client/rsg-components/VsgReactComponent/ReactComponent.tsx
+++ packages/vue-styleguidist/src/client/rsg-components/VsgReactComponent/ReactComponent.tsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import * as Rsg from 'react-styleguidist'
 import RsgReactComponent from 'react-styleguidist/lib/client/rsg-components/ReactComponent/ReactComponent'
+
+interface ReactComponentProps {
+	component: Rsg.Component & { props: { subComponents?: Rsg.Component[] } }
+	depth: number
+	exampleMode?: string
+	usageMode?: string
+}
 
 export const DocumentedComponentContext = React.createContext({})
 
-export default class VsgReactComponent extends Component {
-	static propTypes = RsgReactComponent.PropTypes
+export default class VsgReactComponent extends Component<ReactComponentProps> {
+	static propTypes = RsgReactComponent.propTypes
 
 	render() {
 		return (
@@ -14,8 +22,11 @@ export default class VsgReactComponent extends Component {
 				</DocumentedComponentContext.Provider>
 				{this.props.component.props.subComponents ? (
 					<div>
-						{this.props.component.props.subComponents.map(c => (
-							<DocumentedComponentContext.Provider key={c.props.displayName} value={c}>
+						{this.props.component.props.subComponents.map((c, i) => (
+							<DocumentedComponentContext.Provider
+								key={(c.props && c.props.displayName) || i}
+								value={c}
+							>
 								<RsgReactComponent {...this.props} component={c} depth={this.props.depth + 1} />
 							</DocumentedComponentContext.Provider>
 						))}
