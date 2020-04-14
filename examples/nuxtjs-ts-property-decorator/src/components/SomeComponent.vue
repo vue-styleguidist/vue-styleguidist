4ca38bf6a6e5fd39e87276332461c24ddb01cc5a
simsim0709 <simsim0709@gmail.com>
2020-04-14 15:02:01 +0900

    fix(docgen):  fix ts array, intersection type print

--- examples/nuxtjs-ts-property-decorator/src/components/SomeComponent.vue
+++ examples/nuxtjs-ts-property-decorator/src/components/SomeComponent.vue
@@ -5,25 +5,33 @@
 </template>
 
 <script lang="ts">
-
 import { Vue, Component, Prop } from 'nuxt-property-decorator'
+
+interface Book {
+	name: string
+	desc: string
+}
 
 @Component
 export default class SomeComponent extends Vue {
 	/** Description for propA */
-	@Prop({ type: Number, default: 3}) readonly propA!: number
+	@Prop({ default: 3 })
+	readonly propA!: 'string literal' | 3 | Book | string[] | number[] | Book[] | Array<Book>
 
-  /**
-   * Description for mwthod. Don't froget to put `@public`.
+	@Prop({ default: 3 })
+	readonly propB!: 'string literal' & 3 & Book & string[] & number[] & Book[] & Array<Book>
+
+	/**
+	 * Description for mwthod. Don't froget to put `@public`.
 	 * @public
-   */
+	 */
 	public onClick(name: string) {
-    /**
-     * Success event when we click.
+		/**
+		 * Success event when we click.
 		 * @property {string} name description of name.
-     * @property {boolean} success always true.
-     */
-    this.$emit('success', { name, success: true })
+		 * @property {boolean} success always true.
+		 */
+		this.$emit('success', { name, success: true })
 	}
 }
 </script>
