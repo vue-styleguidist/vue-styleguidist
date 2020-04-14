d2103febadf74722945642dfefc0f4f0cdf493ac
elevatebart <ledouxb@me.com>
2020-04-14 16:18:49 -0500

    feat: use pug-loader options to fuel docgen

--- packages/vue-styleguidist/src/loaders/utils/getParser.ts
+++ packages/vue-styleguidist/src/loaders/utils/getParser.ts
@@ -1,4 +1,5 @@
 import { parse, ComponentDoc } from 'vue-docgen-api'
+import * as pug from 'pug'
 import { SanitizedStyleguidistConfig } from '../../types/StyleGuide'
 import mergeWebpackConfig from '../../scripts/utils/mergeWebpackConfig'
 
@@ -14,16 +15,32 @@ export default function getParser(
 
 	let alias: { [key: string]: string } | undefined
 	let modules: string[] | undefined
+
 	if (webpackConfig.resolve) {
 		alias = webpackConfig.resolve.alias
 		modules = webpackConfig.resolve.modules
 	}
+
+	let pugOptions: pug.Options | undefined
+	if (webpackConfig.rules) {
+		const pugLoader =
+			webpackConfig.rules.find((r: any) => r.loader === 'pug-loader') ||
+			webpackConfig.rules.find((r: any) => r.use.loader === 'pug-loader').use ||
+			webpackConfig.rules
+				.filter((r: any) => Array.isArray(r.use))
+				.find((r: any) => r.loader === 'pug-loader')
+		if (pugLoader) {
+			pugOptions = pugLoader.options
+		}
+	}
+
 	const defaultParser = async (file: string) =>
 		await parse(file, {
 			alias,
 			modules,
 			jsx: config.jsxInComponents,
-			validExtends: config.validExtends
+			validExtends: config.validExtends,
+			pugOptions
 		})
 	return config.propsParser || defaultParser
 }
