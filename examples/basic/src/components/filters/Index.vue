d2378493f25891013145bb868bb0993c7327aead
elevatebart <ledouxb@me.com>
2020-04-20 19:19:21 -0500

    docs: upd basic example with displayName example

---
+++ examples/basic/src/components/filters/Index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Input />
+    <slot />
+  </div>
+</template>
+<script>
+import Input from '../Input/Input.vue'
+import { logger } from '../../mixins/loggerMixin'
+
+/**
+ * @displayName notnot
+ */
+export default {
+	components: { Input },
+	mixins: [logger]
+}
+</script>
---
+++ packages/vue-docgen-api/tests/components/filters/Index.vue
@@ -0,0 +1,17 @@
+<template>
+	<div>
+		<Icon />
+		<slot />
+	</div>
+</template>
+<script>
+import Icon from '@/components/Icon'
+import { cart } from 'my-cart'
+
+/**
+ * @displayName NotFilters
+ */
+export default {
+	components: { Icon }
+}
+</script>
