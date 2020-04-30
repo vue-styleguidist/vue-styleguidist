07f6a98dc391a02d503c62debcfd3b8b7950d0e9
elevatebart <ledouxb@me.com>
2020-04-30 10:31:16 -0500

    fix(plugin): update null-loader + avoid conflicts

--- packages/vue-cli-plugin-styleguidist/index.js
+++ packages/vue-cli-plugin-styleguidist/index.js
@@ -1,6 +1,7 @@
 const vsg = require('vue-styleguidist')
 const merge = require('webpack-merge')
 const configSchemaImport = require('vue-styleguidist/lib/scripts/schemas/config')
+const NullLoader = require('null-loader').default
 
 const configSchema = configSchemaImport.default || configSchemaImport
 const styleguidist = vsg.default || vsg
@@ -12,8 +13,8 @@ module.exports = (api, options) => {
 		module: {
 			rules: [
 				{
-					resourceQuery: /blockType=docs/,
-					loader: 'null-loader'
+					loader: NullLoader,
+					resourceQuery: /blockType=docs/
 				}
 			]
 		}
