ec5bb246bca5549408f8a545c0b5be4aeb2efbb5
elevatebart <ledouxb@gmail.com>
2020-07-28 14:42:20 -0500

    style(cli): add a comment on the compile function

--- packages/vue-docgen-cli/src/compileTemplates.ts
+++ packages/vue-docgen-cli/src/compileTemplates.ts
@@ -33,11 +33,15 @@ export function getDependencies(doc: Pick<ComponentDoc, 'tags'>, compDirName: st
 }
 
 /**
- *
+ * Umbrella that calls docgen
+ * Calls each template provided by the user
+ * And generates a markdown string + the list of dependencies
  * @param absolutePath
  * @param config
  * @param componentRelativePath
  * @param extraMd
+ * @returns content will contain the markdown text, dependencies contains
+ * an array of path to files that should trigger the update of this file
  */
 export default async function compileTemplates(
 	absolutePath: string,
