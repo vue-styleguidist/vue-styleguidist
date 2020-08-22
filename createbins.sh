a1bf8325843b429b7e55715a19d8e6a3e855d35c
elevatebart <ledouxb@gmail.com>
2020-08-21 20:30:25 -0500

    build: make toucher work fine

--- createbins.sh
+++ createbins.sh
@@ -1,2 +1,5 @@
-touch packages/vue-styleguidist/lib/bin/styleguidist.js
-touch packages/vue-docgen-cli/lib/bin.js
+touch2() { mkdir -p "$(dirname "$1")" && touch "$1" ; }
+
+touch2 packages/vue-styleguidist/lib/bin/styleguidist.js
+touch2 packages/vue-styleguidist/lib/scripts/index.js
+touch2 packages/vue-docgen-cli/lib/bin.js
