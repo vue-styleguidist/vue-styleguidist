2f6289e59886ed81af66a260009e70af83ba65f7
elevatebart <ledouxb@gmail.com>
2020-10-05 17:59:31 -0500

    docs: add an example with nuxt content

---
+++ examples/docgen-nuxt/components/Button/Second-ReadMe.md
@@ -0,0 +1,3 @@
+## This is the second readme
+
+one can also load markdown using the `@examples` doclets
---
+++ examples/docgen-nuxt/components/DropDown/MyOption.vue
@@ -0,0 +1,20 @@
+<template>
+	<option :value="val">
+		<!-- @slot text displayed in the option -->
+		<slot />
+	</option>
+</template>
+<script>
+export default {
+	name: 'MyOption',
+	props: {
+		/**
+		 * value associated with the option
+		 */
+		val: {
+			type: String,
+			required: true
+		}
+	}
+}
+</script>
