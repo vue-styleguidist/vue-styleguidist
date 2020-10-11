a702419226b2546aa49a33eaaf7bbbc764e0ad9a
ElevateBart <ledouxb@gmail.com>
2020-10-11 14:55:46 -0500

    docs: build docgen example

--- examples/docgen/docs/.vuepress/config.js
+++ examples/docgen/docs/.vuepress/config.js
@@ -4,28 +4,28 @@ const cwd = path.join(__dirname, '..')
 const { parse } = require('vue-docgen-api')
 
 module.exports = async () => {
-	const sidebar = glob.sync('components/**/*.md', { cwd }).map((f) => '/' + f)
+	const sidebar = glob.sync('components/**/*.md', { cwd }).map(f => '/' + f)
 	const components = await Promise.all(
-		glob.sync('../src/components/**/*.{vue,js,jsx,ts,tsx}', { cwd, absolute: true }).map(async (path) => {
-			return {
-				name: (await parse(path)).displayName.replace(/[^a-zA-Z0-9_]/g, ''),
-				path
-			}
-		})
+		glob
+			.sync('../src/components/**/*.{vue,js,jsx,ts,tsx}', { cwd, absolute: true })
+			.map(async path => {
+				return {
+					name: (await parse(path)).displayName.replace(/[^a-zA-Z0-9_]/g, ''),
+					path
+				}
+			})
 	)
 
 	return {
-		base: "/docgen/",
-		head: [
-			['link', { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-		],
+		base: '/docgen/',
+		head: [['link', { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]],
 		dest: path.join(__dirname, '../../dist'),
 		title: 'VuePress DocGen Live',
 		themeConfig: {
 			sidebar
 		},
 		plugins: [
-			['live'],
+			['live', { noSsr: true }],
 			[
 				'@vuepress/register-components',
 				{
