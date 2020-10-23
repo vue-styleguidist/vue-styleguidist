a062a5405b9793e46bf33ec49a6f3d6511fb7e28
ElevateBart <ledouxb@gmail.com>
2020-10-23 17:10:27 -0500

    fix(compiler): only allow max version of each bro

--- packages/vue-inbrowser-compiler/src/getTargetFromBrowser.ts
+++ packages/vue-inbrowser-compiler/src/getTargetFromBrowser.ts
@@ -3,14 +3,25 @@ import { detect } from 'detect-browser'
 
 const browser = detect()
 
+const BROWSERS = {
+	chrome: 71,
+	firefox: 64,
+	safari: 12,
+	ie: 11,
+	edge: 19
+}
+
 function isBubleBrowser(name: string): name is 'chrome' | 'firefox' | 'safari' | 'ie' | 'edge' {
-	return ['chrome', 'firefox', 'safari', 'ie', 'edge'].includes(name)
+	return name in BROWSERS
 }
 
 export default function getTargetFromBrowser(): TransformOptions['target'] {
 	if (browser?.version && browser?.name) {
 		if (isBubleBrowser(browser.name)) {
-			return { [browser.name]: browser.version }
+			const version = parseInt(browser.version, 10)
+			return {
+				[browser.name]: version <= BROWSERS[browser.name] ? version : BROWSERS[browser.name]
+			}
 		}
 	}
 	return {}
