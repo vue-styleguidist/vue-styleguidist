01f4fda3cb634f994c65c883d7b4ae87fcc363db
ElevateBart <ledouxb@gmail.com>
2020-11-02 14:14:50 -0600

    refactor: avoid using require

--- packages/vue-inbrowser-compiler-utils/src/adaptCreateElement.ts
+++ packages/vue-inbrowser-compiler-utils/src/adaptCreateElement.ts
@@ -1,5 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const camelCase = require('camelcase')
+import camelCase from 'camelcase'
 
 export type CreateElementFunction = (
 	component: string | object,
