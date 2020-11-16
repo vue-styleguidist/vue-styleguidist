73a5c58229dd7ce9eb75895ad106132c8c2f186f
ElevateBart <ledouxb@gmail.com>
2020-11-16 09:25:21 -0600

    fix(cli): simplify bin for docgen

--- packages/vue-docgen-cli/src/bin.ts
+++ packages/vue-docgen-cli/src/bin.ts
@@ -1,31 +1,3 @@
 #!/usr/bin/env node
 
-import minimist from 'minimist'
-import extractConfig from './extractConfig'
-import docgen from './docgen'
-import { SafeDocgenCLIConfig } from './config'
-
-/**
- * run the `config` recursively on pages
- * @param config
- */
-function run(config: SafeDocgenCLIConfig) {
-	const { pages } = config
-	if (pages) {
-		// to avoid re-rendering the same pages
-		delete config.pages
-		pages.forEach(page => {
-			const pageConf = { ...config, ...page }
-			run(pageConf)
-		})
-	} else {
-		docgen(config)
-	}
-}
-
-const { _: pathArray, configFile, watch, cwd } = minimist(process.argv.slice(2), {
-	alias: { c: 'configFile', w: 'watch' }
-})
-
-const conf = extractConfig(cwd || process.cwd(), watch, configFile, pathArray)
-run(conf)
+import './index'
