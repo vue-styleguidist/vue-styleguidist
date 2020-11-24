5e4c0272e1f5b12a7223ad52055f68871525e27f
ElevateBart <ledouxb@gmail.com>
2020-11-23 21:53:11 -0600

    feat(cli): render props and bindings properly
    closes #1013

--- examples/docgen-nuxt/components/CounterButton/CounterButton.vue
+++ examples/docgen-nuxt/components/CounterButton/CounterButton.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="CounterButton">
-		<button class="button" @click.prevent="increment">
+		<button @click.prevent="increment">
 			{{ count }}
 		</button>
 	</div>
@@ -12,26 +12,38 @@
  * @position 2
  */
 export default {
-  name: 'CounterButton',
-  data() {
-    return { count: 0 }
-  },
-  methods: {
-    /**
-     * Increments the counter. This method is not marked @public and is not visible in the styleguide.
-     */
-    increment() {
-      this.count++
-      /**
-       * After increment event
-       * @event after
-       * @type {number}
-       */
-      this.$emit('after', this.count)
-    }
-  }
+	name: 'CounterButton',
+	data() {
+		return { count: 0 }
+	},
+	methods: {
+		/**
+		 * Increments the counter. This method is not marked @public and is not visible in the styleguide.
+		 */
+		increment() {
+			this.count++
+			/**
+			 * After increment event
+			 * @event after
+			 * @property {number} count the count
+			 * @property {boolean} second another parameter
+			 */
+			this.$emit('after', this.count, false)
+		}
+	}
 }
 </script>
+
+<style scoped>
+button {
+	background: #fbfbfb;
+	border: 1px solid #666;
+	border-radius: 6px;
+	padding: 5px;
+	width: 100px;
+}
+</style>
+
 <docs>
 Don't forget that you can debug it with [vue-devtools](https://github.com/vuejs/vue-devtools)
 
--- packages/vue-docgen-cli/src/templates/events.ts
+++ packages/vue-docgen-cli/src/templates/events.ts
@@ -2,12 +2,27 @@ import { EventDescriptor } from 'vue-docgen-api'
 import { mdclean } from './utils'
 import { SubTemplateOptions } from '../compileTemplates'
 
+function formatProperties(properties: EventDescriptor['properties']): string {
+	if (!properties) {
+		return ''
+	}
+	return properties
+		.map(property => {
+			const { name, description, type } = property
+			if (!type) {
+				return ''
+			}
+			return `**${name}** \`${type.names.length ? type.names.join(', ') : ''}\` - ${description}`
+		})
+		.join('\n')
+}
+
 const tmpl = (events: EventDescriptor[]) => {
 	let ret = ''
 	events.forEach(evt => {
 		const { description = '', ...e } = evt
-		const t = e.type && e.type.names ? e.type.names.join(' ') : ''
-		ret += `| ${mdclean(e.name)} | ${mdclean(t)} | ${mdclean(description)}\n`
+		const readableProperties = e.properties ? `${formatProperties(e.properties)}` : ''
+		ret += `| ${mdclean(e.name)} | ${mdclean(readableProperties)} | ${mdclean(description)}\n`
 	})
 	return ret
 }
@@ -16,8 +31,8 @@ export default (events: EventDescriptor[], opt: SubTemplateOptions = {}): string
 	return `
 ${opt.isSubComponent || opt.hasSubComponents ? '#' : ''}## Events
 
-  | Event name     | Type        | Description  |
-  | ------------- |------------- | -------------|
+  | Event name     | Properties     | Description  |
+  | -------------- |--------------- | -------------|
   ${tmpl(events)}
   `
 }
