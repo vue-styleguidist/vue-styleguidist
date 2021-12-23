af48d130dd74b89de7524fa9640f234f7e83382c
ElevateBart <ledouxb@gmail.com>
2021-12-23 15:43:23 -0600

    test: fix setup test case issue

--- packages/vue-docgen-api/tests/components/setup-syntax/setup-syntax.test.ts
+++ packages/vue-docgen-api/tests/components/setup-syntax/setup-syntax.test.ts
@@ -2,7 +2,7 @@ import * as path from 'path'
 import { ComponentDoc } from '../../../src/Documentation'
 import { parse } from '../../../src/main'
 
-const progressPath = path.join(__dirname, './progress.vue')
+const progressPath = path.join(__dirname, './Progress.vue')
 let doc: ComponentDoc
 
 describe('setup syntactic sugar', () => {
