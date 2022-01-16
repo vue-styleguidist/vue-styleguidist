127f288bc2ed26f71629918d2494120c198d73bc
ElevateBart <ledouxb@gmail.com>
2022-01-16 14:18:21 -0600

    feat(docgen): add setup syntax api support

---
+++ packages/vue-docgen-api/src/script-setup-handlers/utils/tsUtils.ts
@@ -0,0 +1,21 @@
+import * as bt from '@babel/types'
+import { NodePath } from 'ast-types/lib/node-path'
+import { visit } from 'recast'
+
+export function getTypeDefinitionFromIdentifier(
+	astPath: bt.File,
+	typeName: string
+): NodePath | undefined {
+	let typeBody: NodePath | undefined = undefined
+	visit(astPath.program, {
+		visitTSInterfaceDeclaration(nodePath) {
+			if (bt.isIdentifier(nodePath.node.id) && nodePath.node.id.name === typeName) {
+				typeBody = nodePath.get('body', 'body')
+			}
+			return false
+		}
+	})
+	return typeBody
+}
+
+export default {}
