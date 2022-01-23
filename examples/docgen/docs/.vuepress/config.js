7454f88d4039d929cce6ae54ac8bef9ee4a118c1
ElevateBart <ledouxb@gmail.com>
2022-01-23 14:54:47 -0600

    chore: make docgen example work (without live)

--- examples/docgen/docs/.vuepress/config.js
+++ examples/docgen/docs/.vuepress/config.js
@@ -25,13 +25,13 @@ module.exports = async () => {
 			sidebar
 		},
 		plugins: [
-			['live', { noSsr: true }],
-			[
-				'@vuepress/register-components',
-				{
-					components
-				}
-			]
+			// ['live', { noSsr: true }],
+			// [
+			// 	'@vuepress/register-components',
+			// 	{
+			// 		components
+			// 	}
+			// ]
 		]
 	}
 }
