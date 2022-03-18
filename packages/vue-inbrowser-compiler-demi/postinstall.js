1dd108a5031993755c24d0079e4281656904987d
ElevateBart <ledouxb@gmail.com>
2022-03-17 22:33:43 -0500

    fix: fix postinstall script

--- packages/vue-inbrowser-compiler-demi/postinstall.js
+++ packages/vue-inbrowser-compiler-demi/postinstall.js
@@ -7,18 +7,28 @@ function updateIndexForVue3() {
 	const indexPath = path.join(__dirname, './index.cjs.js')
 	const indexContent = `
   const Vue = require('vue')
+  
   module.exports.h = Vue.h
   module.exports.resolveComponent = Vue.resolveComponent
   module.exports.isVue3 = true
   `
-	fs.writeFile(indexPath, indexContent)
+	fs.writeFile(indexPath, indexContent, err => {
+		if (err) {
+			console.error(err)
+		}
+	})
 
 	// esm
 	const indexPathESM = path.join(__dirname, './index.esm.js')
 	const indexContentESM = `
   export { h, resolveComponent } from 'vue'
-  export const isVue3 = true`
-	fs.writeFile(indexPathESM, indexContentESM)
+  export const isVue3 = true
+  `
+	fs.writeFile(indexPathESM, indexContentESM, err => {
+		if (err) {
+			console.error(err)
+		}
+	})
 }
 
 if (pkg.version.startsWith('3.')) {
