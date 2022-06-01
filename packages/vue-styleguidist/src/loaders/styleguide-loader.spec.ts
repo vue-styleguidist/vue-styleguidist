ab288f489278fb4aa4569141becb44f6c92d0db9
elevatebart <ledouxb@gmail.com>
2022-06-01 17:15:15 -0500

    now all pass

--- packages/vue-styleguidist/src/loaders/styleguide-loader.spec.ts
+++ packages/vue-styleguidist/src/loaders/styleguide-loader.spec.ts
@@ -2,16 +2,17 @@ import vm from 'vm'
 import path from 'path'
 import * as styleguideLoader from './styleguide-loader'
 
-vi.mock('react-styleguidist/lib/loaders/utils/getComponentFilesFromSections', () => () => [
-	'foo',
-	'bar'
-])
+vi.mock('react-styleguidist/lib/loaders/utils/getComponentFilesFromSections', () => ({
+	default: () => ['foo', 'bar']
+}))
 
-vi.mock('../utils/getSections', () => () => [])
+vi.mock('./utils/getSections', () => ({
+	default: () => []
+}))
 
 describe('styleguide-loader', () => {
-	const file = path.resolve(__dirname, '../../../../../test/components/Button.vue')
-	const configDir = path.resolve(__dirname, '../../../../../test')
+	const file = path.resolve(__dirname, '../../../../test/components/Button.vue')
+	const configDir = path.resolve(__dirname, '../../../../test')
 
 	it('should return valid, parsable JS', () => {
 		const callback = (err: string, result: string) => {
