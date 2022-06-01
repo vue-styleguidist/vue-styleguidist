5b3e26de97147f28b176db16cf69e196bda0afa9
elevatebart <ledouxb@gmail.com>
2022-06-01 12:20:35 -0500

    remove tests from client (to replace with cy)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vitest/config'
+import { typescriptPaths } from 'rollup-plugin-typescript-paths'
 
 export default defineConfig({
-  test: {
-    globals: true
-  }
+	test: {
+		globals: true
+	},
+	plugins: [
+		typescriptPaths({
+			tsConfigPath: './tsconfig.json'
+		})
+	]
 })
