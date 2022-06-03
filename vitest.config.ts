34714768df8179f93bca019c2c4b76a1f4197c1c
Bart Ledoux <ledouxb@gmail.com>
2022-06-02 23:47:12 -0500

    tests: remove tip from log

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
 	test: {
 		environment: 'happy-dom',
 		globals: true,
+    setupFiles: './test/setup.ts',
 		exclude: ['**/node_modules/**', '**/lib/**', '**/dist/**']
 	},
 	plugins: [
