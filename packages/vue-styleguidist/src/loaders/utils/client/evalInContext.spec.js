fb8688e995f5a41c0eeaff45199d0d537e76cccb
elevatebart <ledouxb@gmail.com>
2022-06-06 13:32:59 -0500

    make linter ok

--- packages/vue-styleguidist/src/loaders/utils/client/evalInContext.spec.js
+++ packages/vue-styleguidist/src/loaders/utils/client/evalInContext.spec.js
@@ -1,6 +1,13 @@
+/* eslint-disable es5/no-modules,es5/no-block-scoping */
 import evalInContext from './evalInContext'
 
-it('should return a function', function(){
-	const result = evalInContext('alert("header");', function(a){return a}, 'alert("code");')
+it('should return a function', function () {
+	const result = evalInContext(
+		'alert("header");',
+		function (a) {
+			return a
+		},
+		'alert("code");'
+	)
 	expect(typeof result).toBe('function')
 })
--- packages/vue-styleguidist/src/loaders/utils/client/requireInRuntime.spec.js
+++ packages/vue-styleguidist/src/loaders/utils/client/requireInRuntime.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable es5/no-modules,es5/no-block-scoping */
 import requireInRuntime from './requireInRuntime'
 
 const map = {
@@ -5,12 +6,14 @@ const map = {
 	b: 43
 }
 
-it('should return a module from the map', function(){
+it('should return a module from the map', function () {
 	const result = requireInRuntime(map, null, 'a')
 	expect(result).toBe(map.a)
 })
 
-it('should throw if module is not in the map', function(){
-	const fn = function(){return requireInRuntime(map, null, 'c')}
+it('should throw if module is not in the map', function () {
+	const fn = function () {
+		return requireInRuntime(map, null, 'c')
+	}
 	expect(fn).toThrowError('require() statements can be added')
 })
