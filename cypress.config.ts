ae0b7b6f0f985040c6956ec8ea9ab7298aaac960
elevatebart <ledouxb@gmail.com>
2022-07-13 13:52:50 -0500

    add cypress CT to styleguidist

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,13 +1,27 @@
 import { defineConfig } from 'cypress'
+import makeWebpackConfig from './packages/vue-styleguidist/src/scripts/make-webpack-config'
+import getConfig from './packages/vue-styleguidist/src/scripts/config'
+
+const webpackConfig = async () => await makeWebpackConfig(await getConfig({}), 'development')
 
 export default defineConfig({
-  fixturesFolder: false,
-  screenshotsFolder: 'test/cypress/screenshots',
-  videosFolder: 'test/cypress/videos',
-  video: false,
-  e2e: {
-    baseUrl: 'http://localhost:6060',
-    specPattern: 'test/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
-    supportFile: false,
-  },
+	fixturesFolder: false,
+	screenshotsFolder: 'test/cypress/screenshots',
+	videosFolder: 'test/cypress/videos',
+	video: false,
+
+	e2e: {
+		baseUrl: 'http://localhost:6060',
+		specPattern: 'test/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
+		supportFile: false
+	},
+
+	component: {
+		specPattern: 'packages/vue-styleguidist/src/client/**/*.cy.{js,jsx,ts,tsx}',
+		devServer: {
+			framework: 'vue',
+			bundler: 'webpack',
+			webpackConfig
+		}
+	}
 })
