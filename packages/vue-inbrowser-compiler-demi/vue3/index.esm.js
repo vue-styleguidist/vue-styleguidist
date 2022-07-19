a5e2b059562a4102b69b981d9dfd0c6f94ac6e85
elevatebart <ledouxb@gmail.com>
2022-07-19 14:14:17 -0500

    add the necessary id for esm vue3

--- packages/vue-inbrowser-compiler-demi/vue3/index.esm.js
+++ packages/vue-inbrowser-compiler-demi/vue3/index.esm.js
@@ -2,4 +2,5 @@ import { compileTemplate as compile } from '@vue/compiler-sfc'
 export { h, resolveComponent, createApp } from 'vue'
 export const isVue3 = true
 export const Vue2 = () => {}
-export const compileTemplate = template => compile(template).code
+export const compileTemplate = template =>
+	compile({ source: template, filename: '<Example>.vue', id: '' }).code
