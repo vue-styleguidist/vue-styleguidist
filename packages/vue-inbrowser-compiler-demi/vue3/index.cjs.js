ba70a01e7bc8d3bf3c64b3b356a93ef75396960a
elevatebart <ledouxb@gmail.com>
2022-07-19 17:11:06 -0500

    fix cjs compileTemplate vue3

--- packages/vue-inbrowser-compiler-demi/vue3/index.cjs.js
+++ packages/vue-inbrowser-compiler-demi/vue3/index.cjs.js
@@ -6,4 +6,5 @@ module.exports.createApp = Vue.createApp
 module.exports.resolveComponent = Vue.resolveComponent
 module.exports.isVue3 = true
 module.exports.Vue2 = function () {}
-module.exports.compileTemplate = template => VueCompilerSfc.compileTemplate(template).code
+module.exports.compileTemplate = template =>
+	VueCompilerSfc.compileTemplate({ source: template, filename: '<Example>.vue', id: '' }).code
