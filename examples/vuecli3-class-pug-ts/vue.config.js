b608738fee8997e7e033dae3f3f40f794c389265
elevatebart <ledouxb@gmail.com>
2022-08-01 08:12:46 -0500

    fix: add missing env definition

--- examples/vuecli3-class-pug-ts/vue.config.js
+++ examples/vuecli3-class-pug-ts/vue.config.js
@@ -1,9 +1,3 @@
-const path = require('path')
-const cliPath = require.resolve('@vue/cli-service')
-console.log(path.dirname(cliPath))
-const webpackPath = require.resolve('webpack', { paths: [path.dirname(cliPath)] })
-process.env.VSG_WEBPACK_PATH = webpackPath
-
 module.exports = {
 	lintOnSave: false
 }
