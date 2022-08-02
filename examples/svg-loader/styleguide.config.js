4a38f147789f95daed16f6bd78e2e5a5f96c2fcf
elevatebart <bart@cypress.io>
2022-08-02 13:27:58 -0500

    docs: fix svg-loader example

--- examples/svg-loader/styleguide.config.js
+++ examples/svg-loader/styleguide.config.js
@@ -1,7 +1,14 @@
 const docSiteUrl = process.env.DEPLOY_PRIME_URL || 'https://vue-styleguidist.github.io'
+const { defineConfig } = require('vue-styleguidist')
 
-/** @type import("vue-styleguidist").Config */
-module.exports = {
+const path = require('path')
+
+const cliPath = require.resolve('@vue/cli-service')
+console.log(path.dirname(cliPath))
+const webpackPath = require.resolve('webpack', { paths: [path.dirname(cliPath)] })
+process.env.VSG_WEBPACK_PATH = webpackPath
+
+module.exports = defineConfig({
 	// set your styleguidist configuration here
 	title: 'Default Style Guide',
 	components: 'src/components/**/[A-Z]*.vue',
@@ -20,4 +27,4 @@ module.exports = {
 	//   // custom config goes here
 	// }
 	styleguideDir: 'dist'
-}
+})
