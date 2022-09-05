2906faeb4acc2f837322b3374ff811c3dad2916b
Bart Ledoux <ledouxb@gmail.com>
2022-09-05 22:21:50 +0200

    move cypress component tests to vsg

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,42 +1,5 @@
 import { defineConfig } from 'cypress'
 
-const webpackConfig = {
-	resolve: {
-		extensions: ['.ts', '.tsx', '.js', '.jsx'],
-		alias: {
-			'rsg-components': 'react-styleguidist/lib/client/rsg-components'
-		}
-	},
-	module: {
-		rules: [
-      {
-        test: /\.mjs$/,
-        type: 'javascript/auto',
-      },
-			{
-				test: /\.(tsx|ts|js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: [
-							[
-								'@babel/env',
-								{
-									modules: 'commonjs'
-								}
-							],
-							'@babel/typescript',
-							'@babel/react'
-						]
-					}
-				}
-			}
-		]
-	},
-	devtool: 'source-map'
-}
-
 export default defineConfig({
   projectId: "583saw",
 
@@ -49,15 +12,4 @@ export default defineConfig({
 		specPattern: 'test/cypress/integration/**/*.cy.{js,jsx,ts,tsx}',
 		supportFile: false
 	},
-
-	component: {
-		specPattern: 'packages/vue-styleguidist/src/client/**/*.cy.{js,jsx,ts,tsx}',
-		supportFile: 'test/cypress/support/component.tsx',
-		indexHtmlFile: 'test/cypress/support/component-index.html',
-		devServer: {
-			framework: 'react',
-			bundler: 'webpack',
-			webpackConfig
-		}
-	}
 })
