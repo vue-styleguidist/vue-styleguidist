773517559ab90cb9ad65d027e758928339bbfbe4
elevatebart <bart@cypress.io>
2022-10-17 09:34:32 -0500

    make sure the tests are running

--- packages/vue-docgen-i18n/tests/Simple.test.ts
+++ packages/vue-docgen-i18n/tests/Simple.test.ts
@@ -4,12 +4,22 @@ import * as fs from 'fs'
 import * as path from 'path'
 import execa from 'execa'
 
+const cliBinPath = require.resolve('vue-docgen-i18n/lib/bin')
+
 describe('Simple', () => {
 	beforeAll(async () => {
-		await execa('vue-docgen-i18n', ['./tests/__mocks__/Button.vue', '--lang', 'FR'], { cwd: __dirname })
+		await execa(cliBinPath, ['./tests/__mocks__/Button.vue', '--lang', 'FR'], { cwd: __dirname, stdio: 'inherit' })
 	})
 
 	it('should generate a translation file', () => {
 		expect(fs.existsSync(path.join(__dirname, './__mocks__/Button.FR.js'))).toBeTruthy()
 	})
+
+  afterAll(() => {
+    try{
+      fs.unlinkSync(path.join(__dirname, './__mocks__/Button.FR.js'))
+    }catch(e){
+      // eat error
+    }
+  })
 })
