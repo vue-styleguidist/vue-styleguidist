faf2d5972f601a4771b0e48ca2901f7e29fbf941
elevatebart <bart@cypress.io>
2022-10-24 08:59:30 -0500

    avoid re-creating the root every update

--- packages/vue-styleguidist/src/client/index.ts
+++ packages/vue-styleguidist/src/client/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable import/first */
 import 'react-styleguidist/lib/client/polyfills'
 import 'react-styleguidist/lib/client/styles'
-import { createRoot } from 'react-dom/client'
+import { createRoot, Root } from 'react-dom/client'
 import { getParameterByName, hasInHash } from 'react-styleguidist/lib/client/utils/handleHash'
 import renderStyleguide from './utils/renderStyleguide'
 
@@ -31,6 +31,8 @@ const scrollToOrigin = () => {
 	}
 }
 
+let root: Root
+
 const render = () => {
 	// eslint-disable-next-line @typescript-eslint/no-var-requires,import/no-unresolved,import/extensions
 	const styleguide = require('!!../loaders/styleguide-loader!./index.js')
@@ -38,7 +40,9 @@ const render = () => {
 	if (!container) {
 		throw new Error(`Could not find container with id "${styleguide.config.mountPointId}"`)
 	}
-	const root = createRoot(container) // createRoot(container!) if you use TypeScript
+	if (!root) {
+		root = createRoot(container) // createRoot(container!) if you use TypeScript
+	}
 	root.render(renderStyleguide(styleguide, codeRevision))
 }
 
