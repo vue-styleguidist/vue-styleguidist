25b105b8389b70f2c0078a6f9c70c455a4fd3850
Ferdinand Thiessen <f.thiessen@gmx.de>
2023-03-16 23:32:39 +0100

    Update LRU cache dependency to version 8.x (#1506)
    Besides many bug fixes this will improve the performance of the cache a
    lot.
    
    ---------
    
    Signed-off-by: Ferdinand Thiessen <rpm@fthiessen.de>
    Co-authored-by: Barthélémy Ledoux <ledouxb@me.com>
    Co-authored-by: Barthélémy Ledoux <ledouxb@gmail.com>

---
+++ .changeset/pdate-lru.md
@@ -0,0 +1,6 @@
+---
+"vue-docgen-api": patch
+"vue-styleguidist": patch
+---
+
+Update LRU cache dependency to version 8.x
--- @types/lru-cache.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'lru-cache' {
-	class LRUCache {
-		constructor(num: number)
-		get(key: string): any
-		set(key: string, obj: any): void
-	}
-	export = LRUCache
-}
