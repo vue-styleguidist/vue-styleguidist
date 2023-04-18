305faef4ec2f9bf969d465f8dc9daecc2979c6c7
elevatebart <bart@cypress.io>
2023-04-17 22:09:39 -0500

    use the highlighter

--- packages/vue-inbrowser-prismjs-highlighter/rollup.config.mjs
+++ packages/vue-inbrowser-prismjs-highlighter/rollup.config.mjs
@@ -3,8 +3,16 @@ import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 import pkg from './package.json' assert { type: 'json' }
 
-const deps = { ...pkg.dependencies }
-delete deps['ts-map']
+const deps = {
+	...pkg.dependencies,
+	'prismjs/components/prism-clike.js': true,
+	'prismjs/components/prism-markup.js': true,
+	'prismjs/components/prism-javascript.js': true,
+	'prismjs/components/prism-typescript.js': true,
+	'prismjs/components/prism-jsx.js': true,
+	'prismjs/components/prism-tsx.js': true,
+	'prismjs/components/prism-css.js': true
+}
 
 export default {
 	input: './src/index.ts',
@@ -12,21 +20,19 @@ export default {
 		{
 			file: `./dist/index.cjs`,
 			format: 'cjs',
-      sourcemap: true
+			sourcemap: true
 		},
-    {
+		{
 			file: `./dist/index.mjs`,
 			format: 'es',
-      sourcemap: true
+			sourcemap: true
 		}
 	],
 	plugins: [
 		resolve(),
 		commonjs(),
 		typescript({
-			tsconfig: './tsconfig.build.json',
-			declaration: false,
-      sourceMap: true,
+			tsconfig: './tsconfig.build.json'
 		})
 	],
 	external: Object.keys(deps)
