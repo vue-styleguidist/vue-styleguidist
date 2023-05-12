a3118bae16548babf4022ebd37cbb49d80d376a1
elevatebart <bart@cypress.io>
2023-05-12 17:28:44 -0500

    cleanup

---
+++ packages/vue-docgen-api/tests/setup.ts
@@ -0,0 +1,7 @@
+/**
+ * To debug tests of this project in vscode,
+ * you need to open the package as the root of your vscode workspace
+ * The vitest plugin is not smart enough yet to deal with pnpm workspaces
+ * This file implements the vscode workspace root setup in the local run
+ */
+import '../../../test/setup'
