6e42f56193f9343712b3178de77c4223cd2bb241
elevatebart <bart@cypress.io>
2023-05-14 23:27:11 -0500

    make tests pass in isolated mode

--- test/setup.ts
+++ test/setup.ts
@@ -4,21 +4,20 @@ import Vue from 'vue'
 Vue.config.productionTip = false
 Vue.config.devtools = false
 
-import { expect } from 'vitest'
 import { URL } from 'url'
-import {resolve} from 'path'
+import { resolve } from 'path'
 
 const __dirname = new URL('.', import.meta.url).pathname
 
 const rootFolder = resolve(__dirname, '../')
 
 expect.addSnapshotSerializer({
-  serialize(val) {
-    // `printer` is a function that serializes a value using existing plugins.
-    return val.replaceAll(rootFolder, '')
-  },
-  test(val) {
-    return typeof val === 'string' && val.includes(rootFolder)
-  },
+	serialize(val) {
+		// `printer` is a function that serializes a value using existing plugins.
+		return val.replaceAll(rootFolder, '')
+	},
+	test(val) {
+		return typeof val === 'string' && val.includes(rootFolder)
+	}
 })
 
