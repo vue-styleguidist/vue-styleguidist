89792084fa22e2402dfc8c57fc4415b80282d516
elevatebart <bart@cypress.io>
2023-06-13 22:52:51 +0200

    add tests and handler

---
+++ packages/vue-docgen-api/src/script-setup-handlers/setupOptionsHandler.test.ts
@@ -0,0 +1,45 @@
+import { ParserPlugin } from '@babel/parser'
+import * as bt from '@babel/types'
+import { NodePath } from 'ast-types/lib/node-path'
+import babylon from '../babel-parser'
+import Documentation, { ExposeDescriptor } from '../Documentation'
+import resolveExportedComponent from '../utils/resolveExportedComponent'
+import setupOptionsHandler from './setupOptionsHandler'
+
+function parse(src: string, plugins?: ParserPlugin[]): bt.File {
+	return babylon({ plugins }).parse(src)
+}
+
+describe('setupOptionsHandler', () => {
+	let documentation: Documentation
+
+	let stubNodePath: NodePath<any, any> | undefined
+	const options = { filePath: '', validExtends: () => true }
+	beforeAll(() => {
+		const defaultAST = babylon({ plugins: ['typescript'] }).parse('export default {}')
+		stubNodePath = resolveExportedComponent(defaultAST)[0]?.get('default')
+	})
+
+	beforeEach(() => {
+		documentation = new Documentation('test/path')
+	})
+
+	async function parserTest(src: string, plugins: ParserPlugin[] = ['typescript']) {
+		const ast = parse(src, plugins)
+		await setupOptionsHandler(documentation, stubNodePath!, ast, options)
+	}
+
+	it('should resolve name from defineOptions', async () => {
+		const src = `
+        const testProps = 0
+        defineOptions([
+          /**
+           * Overridden component name
+           */
+          name: "testName"
+        ])
+        `
+		await parserTest(src)
+		expect(documentation.get('name')).toEqual('testName')
+	})
+})
---
+++ packages/vue-docgen-api/src/script-setup-handlers/setupOptionsHandler.ts
@@ -0,0 +1,29 @@
+import * as bt from '@babel/types'
+import { NodePath } from 'ast-types/lib/node-path'
+import { visit } from 'recast'
+import Documentation from '../Documentation'
+import type { ParseOptions } from '../types'
+
+/**
+ * Extract information from an setup-style VueJs 3 component
+ * about what events can be emitted
+ * @param {NodePath} astPath
+ * @param {Array<NodePath>} componentDefinitions
+ * @param {string} originalFilePath
+ */
+export default async function setupOptionsHandler(
+	documentation: Documentation,
+	componentDefinition: NodePath,
+	astPath: bt.File,
+	opt: ParseOptions
+) {
+	visit(astPath.program, {
+		visitCallExpression(nodePath) {
+			if (bt.isIdentifier(nodePath.node.callee) && nodePath.node.callee.name === 'defineOptions') {
+				// here be dragons
+			}
+
+			return false
+		}
+	})
+}
