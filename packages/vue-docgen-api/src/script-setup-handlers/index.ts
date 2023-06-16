9a370b0d36b0e0fb371eeba7e174de1516b8eddb
elevatebart <bart@cypress.io>
2023-06-16 16:37:02 +0200

    add it to the standard setup handler

--- packages/vue-docgen-api/src/script-setup-handlers/index.ts
+++ packages/vue-docgen-api/src/script-setup-handlers/index.ts
@@ -1,5 +1,6 @@
 import setupEventHandler from './setupEventHandler'
+import setupOptionsHandler from './setupOptionsHandler'
 import setupPropHandler from './setupPropHandler'
 import setupExposeHandler from './setupExposeHandler'
 
-export default [setupEventHandler, setupPropHandler, setupExposeHandler]
+export default [setupEventHandler, setupOptionsHandler, setupPropHandler, setupExposeHandler]
