5ca41a1fc602997b0ae431781397e7f1ec21e34e
Fabian Becker <fabian.becker@factset.com>
2023-07-18 12:51:40 +0200

    feat(script-setup): support external types

--- packages/vue-docgen-api/src/script-setup-handlers/utils/tsUtils.test.ts
+++ packages/vue-docgen-api/src/script-setup-handlers/utils/tsUtils.test.ts
@@ -1,5 +1,6 @@
 import { getTypeDefinitionFromIdentifier } from './tsUtils'
 import buildParser from '../../babel-parser'
+import { ParseOptions } from '../../types'
 
 describe('getTypeDefinitionFromIdentifier', () => {
 	it('resolves an interface in the global scope', () => {
@@ -15,13 +16,15 @@ describe('getTypeDefinitionFromIdentifier', () => {
 					defineProps<LocalType>()
 					`
 		const astFile = parser.parse(src)
-		//console.log(astFile)
-		const nodePath: any = getTypeDefinitionFromIdentifier(astFile, 'LocalType')
-		const props = nodePath?.map((prop: any) => prop.node.key.name+' '+prop.node.typeAnnotation.typeAnnotation.type); 
-		expect(
-			props
-			).toStrictEqual(["prop1 TSBooleanKeyword","prop2 TSStringKeyword"]);
-	}),
+		const nodePath: any = getTypeDefinitionFromIdentifier(astFile, 'LocalType', {
+			filePath: __filename
+		} as ParseOptions)
+		const props = nodePath?.map(
+			(prop: any) => prop.node.key.name + ' ' + prop.node.typeAnnotation.typeAnnotation.type
+		)
+		expect(props).toStrictEqual(['prop1 TSBooleanKeyword', 'prop2 TSStringKeyword'])
+	})
+
 	it('resolves an Type alias in the global scope', () => {
 		const parser = buildParser({ plugins: ['typescript'] })
 		const src = `
@@ -35,10 +38,29 @@ describe('getTypeDefinitionFromIdentifier', () => {
 					defineProps<LocalType>()
 					`
 		const astFile = parser.parse(src)
-		const nodePath: any = getTypeDefinitionFromIdentifier(astFile, 'LocalType')
-		const props = nodePath?.map((prop: any) => prop.node.key.name+' '+prop.node.typeAnnotation.typeAnnotation.type); 
-		expect(
-			props
-			).toStrictEqual(["prop1 TSBooleanKeyword","prop2 TSStringKeyword"]);
+		const nodePath: any = getTypeDefinitionFromIdentifier(astFile, 'LocalType', {
+			filePath: __filename
+		} as ParseOptions)
+		const props = nodePath?.map(
+			(prop: any) => prop.node.key.name + ' ' + prop.node.typeAnnotation.typeAnnotation.type
+		)
+		expect(props).toStrictEqual(['prop1 TSBooleanKeyword', 'prop2 TSStringKeyword'])
+	})
+
+	it('resolves an Type alias defined in another file', () => {
+		const parser = buildParser({ plugins: ['typescript'] })
+		const src = `
+		import type {LocalType} from './__fixtures__/types.ts';
+			
+		defineProps<LocalType>()
+					`
+		const astFile = parser.parse(src)
+		const nodePath: any = getTypeDefinitionFromIdentifier(astFile, 'LocalType', {
+			filePath: __filename
+		} as ParseOptions)
+		const props = nodePath?.map(
+			(prop: any) => prop.node.key.name + ' ' + prop.node.typeAnnotation.typeAnnotation.type
+		)
+		expect(props).toStrictEqual(['prop1 TSBooleanKeyword', 'prop2 TSStringKeyword'])
 	})
 })
