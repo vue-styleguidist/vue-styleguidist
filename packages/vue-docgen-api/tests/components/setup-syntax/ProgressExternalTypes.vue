1ecc082a069193afeb7bae4493641cd81aa93432
Fabian Becker <fabian.becker@b-it-d.de>
2023-07-25 15:58:36 +0200

    Add initial support for external types in defineProps and defineEmits (#1586)
    This PR adds initial support for imported interfaces and types in
    `defineProps` and `defineEmits`.
    
    Based on my understanding of the codebase, so I might (as usual :) )
    have missed something obvious. I tested the change on a fairly large
    code base (about 200 components + sub-components) and haven't noticed
    any performance degradations.
    
    Known limitations:
    
    * Does not support resolving modules that need additional configuration
    to make sense (`import type {Props} from '@/interfaces.ts'`)
    * Does not support types defined in third-party dependencies / in
    `node_modules`
    
    ---------
    
    Co-authored-by: Fabian Becker <fabian.becker@factset.com>

---
+++ packages/vue-docgen-api/tests/components/setup-syntax/ProgressExternalTypes.vue
@@ -0,0 +1,61 @@
+<template>
+  <svg :height="radius * 2" :width="radius * 2" @click="convertTheThings(1); emit('save', circumference);">
+    <circle stroke="#EBEBEB" fill="transparent" :stroke-width="stroke" :r="normalizedRadius" :cx="radius"
+      :cy="radius" />
+    <circle stroke="currentColor" fill="transparent" :stroke-dasharray="circumference + ' ' + circumference"
+      :style="{ strokeDashoffset }" :stroke-width="stroke" :r="normalizedRadius" :cx="radius" :cy="radius"
+      stroke-linecap="round" />
+  </svg>
+</template>
+
+<script>
+
+function convertTheThings(value) {
+  return value;
+}
+
+export { convertTheThings }
+
+</script>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+import type { PropsInterface } from './interfaces';
+
+const emit = defineEmits<{
+  /**
+   * Cancels everything
+   */
+  (event: 'cancel'): void
+  /**
+   * Save the world
+   */
+  (event: 'save', arg: number): void
+}>()
+
+defineExpose([
+  /**
+   * position of the dash offset
+   */
+  'strokeDashoffset'
+])
+
+const props = withDefaults(defineProps<PropsInterface>(),
+  {
+    progress: 0,
+  })
+
+const normalizedRadius = props.radius - props.stroke * 2
+const circumference = normalizedRadius * 2 * Math.PI
+
+const strokeDashoffset = computed(() => {
+  return circumference - props.progress / 100 * circumference
+})
+</script>
+
+<style scoped>
+svg {
+  transform: rotate(-90deg);
+  transform-origin: 50% 50%;
+}
+</style>
