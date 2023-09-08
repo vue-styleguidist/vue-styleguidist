8a24675db2fecb5efdb7efa52aea1e1962408ffd
elevatebart <bart@cypress.io>
2023-09-08 11:43:02 +0200

    fix: avoid failure and crash on composite props
    closes #1579

---
+++ packages/vue-docgen-api/tests/components/setup-composite-props/example.vue
@@ -0,0 +1,29 @@
+<template>
+	<button type="button">
+		{{ msg }}
+	</button>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue'
+
+type BaseProps = {
+	/**
+	 * The msg of the button
+	 */
+	msg: string
+}
+
+type MyProps = BaseProps & {
+	/**
+	 * primary or secondary button
+	 */
+	primary?: boolean
+	/**
+	 * size of the button
+	 */
+	size?: 'small' | 'medium' | 'large'
+}
+
+const { primary = false, size = 'small', msg } = defineProps<MyProps>()
+</script>
