ad62772f709e53c79aebbb62a6c68b7d438eb21c
Leo Nicolle <leo@linkurio.us>
2023-11-03 13:56:00 +0100

    added a handler to parse @description tags within Single File Components

--- packages/vue-docgen-api/src/script-setup-handlers/index.ts
+++ packages/vue-docgen-api/src/script-setup-handlers/index.ts
@@ -2,5 +2,7 @@ import setupEventHandler from './setupEventHandler'
 import setupOptionsHandler from './setupOptionsHandler'
 import setupPropHandler from './setupPropHandler'
 import setupExposeHandler from './setupExposeHandler'
+import setupDescriptionHandler from './setupDescriptionHandler'
 
-export default [setupEventHandler, setupOptionsHandler, setupPropHandler, setupExposeHandler]
+export default [setupEventHandler, setupOptionsHandler, setupPropHandler, setupExposeHandler,
+  setupDescriptionHandler]
---
+++ packages/vue-docgen-api/src/script-setup-handlers/setupDescriptionHandler.ts
@@ -0,0 +1,38 @@
+import { visit } from 'recast'
+import { defineHandler } from './utils/tsUtils'
+import { parseDocblock } from '../utils/getDocblock'
+import { Node } from '@babel/types'
+/**
+ * Extract description from an setup-style VueJs 3 component
+ * from @ description tag 
+ * @param {NodePath} astPath
+ * @param {Array<NodePath>} componentDefinitions
+ * @param {string} originalFilePath
+ */
+export default defineHandler(async function setupDescriptionHandler(
+	documentation,
+	componentDefinition,
+	astPath,
+) {
+	console.log('SETUP', documentation, astPath);
+	visit(astPath.program, {
+		visitProgram(path) {
+			const body = path.value.body as Node[];
+			const description = body.reduce((acc, node) => {
+				if (acc) return acc;
+				const descriptionComment = node.leadingComments?.find((comment) => {
+					if (!comment.value.includes('@description')) return;
+					return true;
+				});
+				if (!descriptionComment) return acc;
+				return parseDocblock(descriptionComment.value)
+					.replace('@description', '')
+					.replace(/@displayName.*/, '')
+					.trim();
+			}, null)
+			if (!description) return false;
+			documentation.set('description', description);
+			return false;
+		},
+	})
+})
