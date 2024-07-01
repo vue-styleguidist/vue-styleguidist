d90533f20f4f65418f104729925d53e5aa0b559e
Barthélémy Ledoux <bart@cypress.io>
2024-07-01 19:05:44 +0200

    simpler "no fuss" getVueApp

--- packages/vue-styleguidist/src/client/rsg-components/Preview/getVueApp.ts
+++ packages/vue-styleguidist/src/client/rsg-components/Preview/getVueApp.ts
@@ -1,14 +1,14 @@
 import { createApp, Vue2, isVue3 } from 'vue-inbrowser-compiler-utils'
 import { registerGlobalComponents } from '../../utils/globalComponents'
 
-export function getVueApp(component: any, el: HTMLElement, enhancePreviewApp: (app:any) => void) {
-	return isVue3
-		? (() => {
-				const app = createApp(component)
-        enhancePreviewApp(app)
-				registerGlobalComponents(app)
-				app.mount(el)
-				return app
-		  })()
-		: new Vue2(component).$mount(el)
+export function getVueApp(component: any, el: HTMLElement, enhancePreviewApp: (app: any) => void) {
+	if (isVue3) {
+		const app = createApp(component)
+		enhancePreviewApp(app)
+		registerGlobalComponents(app)
+		app.mount(el)
+		return app
+	} else {
+		return new Vue2(component).$mount(el)
+	}
 }
