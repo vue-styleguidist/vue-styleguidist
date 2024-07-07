f463f77cb90bcd5d7bf37771b6cbaf0882c61037
Barthélémy Ledoux <bart@cypress.io>
2024-07-07 16:04:18 +0200

    use pupetteer to test (again)

--- test/browser.mjs
+++ test/browser.mjs
@@ -1,7 +1,6 @@
-/* eslint-disable compat/compat */
 /* eslint-disable no-console */
-import { execa } from 'execa'
-import cypress from 'cypress'
+import puppeteer from 'puppeteer'
+import * as path from 'path'
 
 const args = process.argv.slice(2)
 
@@ -15,26 +14,37 @@ process.on('unhandledRejection', reason => {
 	process.exit(1)
 })
 
-async function run() {
-	const [, results] = await Promise.all([
-		execa('http-server', ['-p', '8282', `./examples/${args[0]}/dist`]).stdout.pipe(process.stdout),
-
-		cypress.run({
-			browser: 'chrome',
-			config: {
-				e2e: {
-					specPattern: `test/cypress/browser/smoke_test.cy.js`,
-					baseUrl: 'http://127.0.0.1:8282'
-				}
-			}
-		})
-	])
-
-	if (results.totalFailed > 0) {
-		process.exit(1)
+async function onerror(err) {
+	console.error(err.stack)
+	if (browser) {
+		await browser.close()
 	}
-
-	process.exit(0)
+	process.exit(1)
 }
 
-run()
+const launchPuppeteer = async () => {
+	browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] })
+	const page = await browser.newPage()
+	await page.setViewport({ width: 1024, height: 768 })
+	page.on('error', onerror)
+	page.on('pageerror', onerror)
+
+	page.on('console', msg => {
+		if (msg.type() !== 'clear') {
+			console.log('PAGE LOG:', msg.text())
+		}
+	})
+
+	const url = /https?/.test(args[0])
+		? args[0]
+		: `file://${path.resolve(`examples/${args[0]}/dist/index.html`)}`
+	await page.goto(url)
+
+	if (args[1]) {
+		await page.screenshot({ path: args[1] })
+	}
+
+	await browser.close()
+}
+
+launchPuppeteer().catch(onerror)
